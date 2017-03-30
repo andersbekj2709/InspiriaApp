51505f0be503d001f0de5998bd6b8e4d887585ba
andersbekj2709 <anders.b.kjolberg@hiof.no>
2017-03-30 13:50:02 +0200

    la til en ny aktivitet, og en den ene knappen fikk en aktivitet.

--- app/src/main/java/gruppe6/inspiria/MainActivity.java
+++ app/src/main/java/gruppe6/inspiria/MainActivity.java
@@ -1,7 +1,9 @@
 package gruppe6.inspiria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToQuiz (View view){
+
+        Intent QuizActivity = new Intent(this, MainActivity.class);
+        startActivity(QuizActivity);
+     }
 }
 //test
